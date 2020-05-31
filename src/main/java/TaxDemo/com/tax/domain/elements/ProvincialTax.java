ed20da683343e1ba0e96c7f05d6c301dc141ea91
Donovan K <donovankutzer@gmail.com>
2020-05-31 15:03:56 -0700

    Added commentation to methods that were altered

--- src/main/java/TaxDemo/com/tax/domain/elements/ProvincialTax.java
+++ src/main/java/TaxDemo/com/tax/domain/elements/ProvincialTax.java
@@ -57,40 +57,43 @@ public class ProvincialTax extends Tax {
 		}
 	}
 
+	// Donovan Ollenberger-Kutzer
     private double britishcolumbia(double grossIncome) {
 		//setting up tax percentages and their respective lower bounds
 		double[] taxPercentages = {0.0506, 0.0770, 0.1050, 0.1229, 0.1470, 0.1680, 0.2050};
 		double[] lowerBounds = {41725.0, 83451.0, 95812.0, 116344.0, 157748.0, 220000.0};
 
-		//provincialTaxBracket = "10.0% [$0 .. $131,220.01)
-		if (grossIncome >= 0.0 && grossIncome < 41725) {
+		//provincialTaxBracket = "5.06% [$0 .. $41725)
+		if (grossIncome >= 0.0 && grossIncome < 41725.00) {
 			return 0.0506 * grossIncome;
 		}
 
-		//provincialTaxBracket = "12.0% [$131,220.01 .. $157,464.01)
-		else if (grossIncome >= 41725.01 && grossIncome < 83451) {
-			return (grossIncome - 41725.01) * .0770 + computeTaxCategoryPay(taxPercentages, lowerBounds, 0);
+		//provincialTaxBracket = "7.7% [$41725 .. $83451.00)
+		else if (grossIncome >= 41725.00 && grossIncome < 83451.00) {
+			return (grossIncome - 41725.00) * .0770 + computeTaxCategoryPay(taxPercentages, lowerBounds, 0);
 		}
 
-		//provincialTaxBracket = "13.0% [$157,464.01 .. $209,952.01)
-		else if (grossIncome >= 83451.01 && grossIncome < 95812) {
-			return (grossIncome - 83451.01) * .1050 + computeTaxCategoryPay(taxPercentages, lowerBounds, 1);
+		//provincialTaxBracket = "10.50% [$83451.00 .. $95812.00)
+		else if (grossIncome >= 83451.00 && grossIncome < 95812.00) {
+			return (grossIncome - 83451.00) * .1050 + computeTaxCategoryPay(taxPercentages, lowerBounds, 1);
 		}
 
-		//provincialTaxBracket = "14.0% [$209,952.01 .. $314,928.00)
-		else if (grossIncome > 95812.01 && grossIncome < 116344) {
-			return (grossIncome - 95812.01) * .1229 + computeTaxCategoryPay(taxPercentages, lowerBounds, 2);
+		//provincialTaxBracket = "12.29% [$95812.01 .. $116344)
+		else if (grossIncome >= 95812.00 && grossIncome < 116344.00) {
+			return (grossIncome - 95812.00) * .1229 + computeTaxCategoryPay(taxPercentages, lowerBounds, 2);
 		}
 
-		else if (grossIncome > 116344.01 && grossIncome < 157748) {
-			return (grossIncome - 116344.01) * .1470 + computeTaxCategoryPay(taxPercentages, lowerBounds, 3);
+		//provincialTaxBracket = "14.70% [116344.00 .. $157748.00)
+		else if (grossIncome >= 116344.00 && grossIncome < 157748.00) {
+			return (grossIncome - 116344.00) * .1470 + computeTaxCategoryPay(taxPercentages, lowerBounds, 3);
 		}
 
-		else if (grossIncome > 157748.01 && grossIncome < 220000) {
-			return (grossIncome - 157748.01) * .1680 + computeTaxCategoryPay(taxPercentages, lowerBounds, 4);
+		//provincialTaxBracket = "16.80% [157748.00 .. $220000.00)
+		else if (grossIncome >= 157748.00 && grossIncome < 220000.00) {
+			return (grossIncome - 157748.00) * .1680 + computeTaxCategoryPay(taxPercentages, lowerBounds, 4);
 		}
 
-		//provincialTaxBracket = "15.0% [$314,928.01 .. )
+		//provincialTaxBracket = "20.5% [$220000.00 .. )
 		else {
 			return (grossIncome - 220000) * .2050 + computeTaxCategoryPay(taxPercentages, lowerBounds, 5);
 		}
--- src/main/java/TaxDemo/com/tax/domain/elements/Tax.java
+++ src/main/java/TaxDemo/com/tax/domain/elements/Tax.java
@@ -2,8 +2,7 @@ package TaxDemo.com.tax.domain.elements;
 
 //line 72 "../../../../../../../ump/tmpjntyadj0/model.ump"
 //line 324 "../../../../../../../ump/tmpjntyadj0/model.ump"
-public abstract class Tax extends Deduction
-{
+public abstract class Tax extends Deduction {
 
 //------------------------
 // MEMBER VARIABLES
@@ -13,79 +12,82 @@ public abstract class Tax extends Deduction
 // CONSTRUCTOR
 //------------------------
 
-public Tax(Employee aEmployee)
-{
- super(aEmployee);
-}
+	public Tax(Employee aEmployee) {
+		super(aEmployee);
+	}
 
 //------------------------
 // INTERFACE
 //------------------------
 
-public void delete()
-{
- super.delete();
-}
+	public void delete() {
+		super.delete();
+	}
 
 
-/**
-* checks acceptable taxpercentage inputs
-* checking is done in two parts
-* @param taxPercentage an array of tax percentages
-* @param depth defines the depth of income category
-* @return accummulated taxes above the depth
-*/
+	/**
+	 * checks acceptable taxpercentage inputs
+	 * checking is done in two parts
+	 *
+	 * @param taxPercentage an array of tax percentages
+	 * @param depth         defines the depth of income category
+	 * @return accummulated taxes above the depth
+	 */
 // line 86 "../../../../../../../ump/tmpjntyadj0/model.ump"
-protected double computeTaxCategoryPay(double [] taxPercentages, double [] lowerBounds, int depth){
- //defensive programming
-		assert isIncreasing( taxPercentages ) == true : "Unacceptable tax brackets -- should be of length 4 and must be in increasing order";
-		assert isIncreasing( lowerBounds ) == true : "Unacceptable tax brackets -- should be of length 4 and must be in increasing order";
+	protected double computeTaxCategoryPay(double[] taxPercentages, double[] lowerBounds, int depth) {
+		//defensive programming
+		assert isIncreasing(taxPercentages) == true : "Unacceptable tax brackets -- should be of length 4 and must be in increasing order";
+		assert isIncreasing(lowerBounds) == true : "Unacceptable tax brackets -- should be of length 4 and must be in increasing order";
 		assert depth >= 0 && depth < 6 : "Acceptable tax depth ranges between [0 .. 3]";
-		
+
 		//computes taxes recursively level by level
-		switch( depth ) {
-			case 0 : 
-				return ( lowerBounds[0] * taxPercentages[0] );
-			case 1 : 
-				return ( lowerBounds[1] - lowerBounds[0] ) * taxPercentages[1]+ computeTaxCategoryPay( taxPercentages, lowerBounds, 0 );
-			case 2 : 
-				return ( lowerBounds[2] - lowerBounds[1] ) * taxPercentages[2] + computeTaxCategoryPay( taxPercentages, lowerBounds, 1 );
-			case 3 : 
-				return ( lowerBounds[3] - lowerBounds[2] ) * taxPercentages[3] + computeTaxCategoryPay( taxPercentages, lowerBounds, 2 );
-			case 4 :
-				return ( lowerBounds[4] - lowerBounds[3] ) * taxPercentages[4] + computeTaxCategoryPay( taxPercentages, lowerBounds, 3 );
-			case 5 :
-				return ( lowerBounds[5] - lowerBounds[4] ) * taxPercentages[5] + computeTaxCategoryPay( taxPercentages, lowerBounds, 4 );
-			default :
+
+		// Edited to accept more tax brackets as BC has 7
+		switch (depth) {
+			case 0:
+				return (lowerBounds[0] * taxPercentages[0]);
+			case 1:
+				return (lowerBounds[1] - lowerBounds[0]) * taxPercentages[1] + computeTaxCategoryPay(taxPercentages, lowerBounds, 0);
+			case 2:
+				return (lowerBounds[2] - lowerBounds[1]) * taxPercentages[2] + computeTaxCategoryPay(taxPercentages, lowerBounds, 1);
+			case 3:
+				return (lowerBounds[3] - lowerBounds[2]) * taxPercentages[3] + computeTaxCategoryPay(taxPercentages, lowerBounds, 2);
+			case 4:
+				return (lowerBounds[4] - lowerBounds[3]) * taxPercentages[4] + computeTaxCategoryPay(taxPercentages, lowerBounds, 3);
+			case 5:
+				return (lowerBounds[5] - lowerBounds[4]) * taxPercentages[5] + computeTaxCategoryPay(taxPercentages, lowerBounds, 4);
+			default:
 				return 0;
 		}
-}
+	}
 
 
-/**
-* checks acceptable taxpercentage inputs
-* checking is done in two parts
-* @param taxPercentage an array of tax percentages
-* @return <code>true</code> if tax percentages are in increasing order, otherwise <code>false</code>
-*/
+	/**
+	 * checks acceptable taxpercentage inputs
+	 * checking is done in two parts
+	 *
+	 * @param taxPercentage an array of tax percentages
+	 * @return <code>true</code> if tax percentages are in increasing order, otherwise <code>false</code>
+	 */
 // line 113 "../../../../../../../ump/tmpjntyadj0/model.ump"
-private boolean isIncreasing(double [] taxPercentage){
- //checks the length of the array (in Canada, size must be 4)
-		if( taxPercentage.length > 7 )
+	// Altered method to allow taxPercentage array to be larger than 4 to allow for BC
+	private boolean isIncreasing(double[] taxPercentage) {
+		//checks the length of the array (in Canada, size must be 4)
+		if (taxPercentage.length > 7)
 			return false;
-		
+
 		//part 2 - checking whether the elemnts of the array is increasing
 		double temp = taxPercentage[0];
-		for( int i = 1; i < taxPercentage.length; i++ ) {
-			
-			if( temp > taxPercentage[i] )
+		for (int i = 1; i < taxPercentage.length; i++) {
+
+			if (temp > taxPercentage[i])
 				return false;
-			
+
 			temp = taxPercentage[i];
 			i++;
 		}
-		
+
 		return true;
-}
+	}
 
 }
